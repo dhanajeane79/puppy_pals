e3e06dbbccf851ef476d050af7abb7079490c502
dhanajeane79 <dhanajeane79@gmail.com>
2023-06-26 11:52:13 -0400

    Rendered the Puppy Data

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
-  const [featPupId, setFeatPupId] = useState(null)
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("puppyList: ", puppyList);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup: ", featuredPup);
 
   return (
     <div className="App">
@@ -16,7 +18,15 @@ function App() {
           {puppy.name}
         </p>
       ))}
-      {featPupId && <p>{featPupId}</p>}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
