96e33a303e336e3032ad49dced26be47c233a699
dhanajeane79 <dhanajeane79@gmail.com>
2023-06-26 11:44:39 -0400

    Added Functionality

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -5,16 +5,18 @@ import {puppyList} from './data.js'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
 
   console.log("puppyList: ", puppyList);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => (
+        <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && <p>{featPupId}</p>}
     </div>
   );
 }
