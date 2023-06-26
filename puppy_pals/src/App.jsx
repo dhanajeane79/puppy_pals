4cfe659114e73703ff3dc644430d7535d85e159f
dhanajeane79 <dhanajeane79@gmail.com>
2023-06-26 11:13:41 -0400

    useState hook

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
+ 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
     <>
