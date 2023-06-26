0a002ddc9e5b5751c9ad0eff49f8e43f5bd359d8
dhanajeane79 <dhanajeane79@gmail.com>
2023-06-26 11:26:41 -0400

    Map Over the Data

--- puppy_pals/src/App.jsx
+++ puppy_pals/src/App.jsx
@@ -3,17 +3,19 @@ import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
- 
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppies)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
-
 export default App
